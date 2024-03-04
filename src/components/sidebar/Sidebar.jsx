cabc4f23a4f588dc17b337c5df3e9abf1e203523
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2024-03-04 15:36:49 +0500

    Add button toggleRightTab and Fix styles for Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import './sidebar.css';
 
 function Sidebar() {
   const [isOpenArray, setIsOpenArray] = useState([true, true, true, true]);
+  const [isRightTabOpen, setIsRightTabOpen] = useState(true);
 
   const toggleDropdown = (index) => {
     const updatedIsOpenArray = [...isOpenArray];
@@ -10,109 +11,138 @@ function Sidebar() {
     setIsOpenArray(updatedIsOpenArray);
   };
 
+  const toggleRightTab = () => {
+    setIsRightTabOpen(!isRightTabOpen);
+  };
+
   return (
     <div className="sidebar">
       <div className="left-tab">
-        <i className="fa-solid fa-bars" />
+        <button
+          className="toggle"
+          type="button"
+          aria-label="toggle"
+          onClick={toggleRightTab}
+        >
+          <i className="fa-solid fa-bars" />
+        </button>
         <i className="fa-solid fa-user" />
       </div>
-      <div className="right-tab">
-        <div className="explorer">Explorer</div>
-        <div className="dropdown">
-          <button type="button" onClick={() => toggleDropdown(0)}>
-            <i className="fa-solid fa-caret-right" />
-            About
-          </button>
-          {isOpenArray[0] && (
-            <div>
-              <ul className="items">
-                <li>
-                  <i className="fa-brands fa-html5" />
-                  <a href="/" aria-label="Home">
-                    index.html
-                  </a>
-                </li>
-                <li>
-                  <i className="fa-brands fa-css3" />
-                  <a href="/experience">experience.css</a>
-                </li>
-                <li>
-                  <i className="fa-brands fa-js" />
-                  <a href="/skills">skills.js</a>
-                </li>
-              </ul>
-            </div>
-          )}
+      {isRightTabOpen && (
+        <div className="right-tab">
+          <div className="explorer">Explorer</div>
+          <div className="dropdown">
+            <button
+              className="caret"
+              type="button"
+              onClick={() => toggleDropdown(0)}
+            >
+              <i className="fa-solid fa-caret-right" />
+              About
+            </button>
+            {isOpenArray[0] && (
+              <div>
+                <ul className="items">
+                  <li>
+                    <i className="fa-brands fa-html5" />
+                    <a href="/" aria-label="Home">
+                      index.html
+                    </a>
+                  </li>
+                  <li>
+                    <i className="fa-brands fa-css3" />
+                    <a href="/experience">experience.css</a>
+                  </li>
+                  <li>
+                    <i className="fa-brands fa-js" />
+                    <a href="/skills">skills.js</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="dropdown">
+            <button
+              className="caret"
+              type="button"
+              onClick={() => toggleDropdown(1)}
+            >
+              <i className="fa-solid fa-caret-right" />
+              Projects
+            </button>
+            {isOpenArray[1] && (
+              <div>
+                <ul className="items">
+                  <li>
+                    <i className="fa-brands fa-react" />
+                    <a href="/todo-list-app">Todo-List-App.jsx</a>
+                  </li>
+                  <li>
+                    <i className="fa-brands fa-react" />
+                    <a href="/cry-for-cypto">Cry-for-Crypto.jsx</a>
+                  </li>
+                  <li>
+                    <i className="fa-brands fa-react" />
+                    <a href="/math-magicians">Math-Magicians.jsx</a>
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-gem" />
+                    <a href="/book-a-motorcycle">Book-A-Motorcycle.rb</a>
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-gem" />
+                    <a href="/spend-smart">Spend-Smart.rb</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="dropdown">
+            <button
+              className="caret"
+              type="button"
+              onClick={() => toggleDropdown(2)}
+            >
+              <i className="fa-solid fa-caret-right" />
+              Contact
+            </button>
+            {isOpenArray[2] && (
+              <div>
+                <ul className="items">
+                  <li>
+                    <i className="fa-brands fa-google" />
+                    <a href="/contact">email.doc</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="dropdown">
+            <button
+              className="caret"
+              type="button"
+              onClick={() => toggleDropdown(3)}
+            >
+              <i className="fa-solid fa-caret-right" />
+              Other
+            </button>
+            {isOpenArray[3] && (
+              <div>
+                <ul className="items">
+                  <li>
+                    <i className="fa-brands fa-readme" />
+                    <a href="/blogs">blogs.md</a>
+                  </li>
+                  <li>
+                    <i className="fa-brands fa-node-js" />
+                    <a href="/startup">startup.json</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="dropdown">
-          <button type="button" onClick={() => toggleDropdown(1)}>
-            <i className="fa-solid fa-caret-right" />
-            Projects
-          </button>
-          {isOpenArray[1] && (
-            <div>
-              <ul className="items">
-                <li>
-                  <i className="fa-brands fa-react" />
-                  <a href="/todo-list-app">Todo-List-App.jsx</a>
-                </li>
-                <li>
-                  <i className="fa-brands fa-react" />
-                  <a href="/cry-for-cypto">Cry-for-Crypto.jsx</a>
-                </li>
-                <li>
-                  <i className="fa-brands fa-react" />
-                  <a href="/math-magicians">Math-Magicians.jsx</a>
-                </li>
-                <li>
-                  <i className="fa-solid fa-gem" />
-                  <a href="/book-a-motorcycle">Book-A-Motorcycle.rb</a>
-                </li>
-                <li>
-                  <i className="fa-solid fa-gem" />
-                  <a href="/spend-smart">Spend-Smart.rb</a>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className="dropdown">
-          <button type="button" onClick={() => toggleDropdown(2)}>
-            <i className="fa-solid fa-caret-right" />
-            Contact
-          </button>
-          {isOpenArray[2] && (
-            <div>
-              <ul className="items">
-                <li>
-                  <i className="fa-brands fa-google" />
-                  <a href="/contact">email.doc</a>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className="dropdown">
-          <button type="button" onClick={() => toggleDropdown(3)}>
-            <i className="fa-solid fa-caret-right" />
-            Other
-          </button>
-          {isOpenArray[3] && (
-            <div>
-              <ul className="items">
-                <li>
-                  <i className="fa-brands fa-readme" />
-                  <a href="/blogs">blogs.md</a>
-                </li>
-                <li>
-                  <i className="fa-brands fa-node-js" />
-                  <a href="/startup">startup.json</a>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
