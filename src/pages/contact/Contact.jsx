8c476036b452e74744f51a1e34af3c3b5d0af9f8
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2024-03-08 22:32:34 +0500

    Create Conact.jsx and it's styles

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,42 @@
+import './contact.css';
+import { useForm, ValidationError } from '@formspree/react';
+
 function Contact() {
+  const [state, handleSubmit] = useForm('xeqyzano');
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
-    <div>Contact</div>
+    <div className="page">
+      <div className="heading">
+        <h1>Get In Touch</h1>
+        <h2>Fill in the form to start a conversation</h2>
+      </div>
+      <div className="contact">
+        <div className="contactDescription">
+          <p>
+            You can contact me with any questions, suggestions, or just to say
+            hi. I am always open to new ideas and collaborations. It can be
+            anything like collaborating on good projects or startups or anime or
+            gaming or anything else.
+          </p>
+        </div>
+        <div className="contactForm">
+          <form onSubmit={handleSubmit}>
+            <input type="user" name="user" placeholder="Name" />
+            <ValidationError prefix="User" field="user" errors={state.errors} />
+
+            <input type="email" name="email" placeholder="Email" />
+            <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+            <textarea name="message" cols="22" rows="5" placeholder="Your Message" />
+            <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+            <button type="submit" disabled={state.submitting}> Submit </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
